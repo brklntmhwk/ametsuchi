3587c221dadeadcfe9d58130ab6be1a028bdf1d3
brklntmhwk <contact@younagi.dev>
2025-08-19 08:42:22 +0000

    chore(flake): add cachix substituter and public key

--- flake.nix
+++ flake.nix
@@ -7,9 +7,11 @@
   nixConfig = {
     extra-substituters = [
       "https://nix-community.cachix.org"
+      "https://brklntmhwk.cachix.org"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "brklntmhwk.cachix.org-1:mGWjznSV6FglvHR7/2sa4MrCtGHMLiAOc9Ru+tEkdyg="
     ];
   };
 
