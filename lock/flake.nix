041c6637d082194f6a3e2e2c51482c0227431674
brklntmhwk <contact@younagi.dev>
2025-08-20 01:11:07 +0000

    chore(deps): create lock entries

---
+++ lock/flake.nix
@@ -0,0 +1,258 @@
+{
+  description = "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
+  inputs = {
+    ace-window = {
+      flake = false;
+      owner = "abo-abo";
+      repo = "ace-window";
+      type = "github";
+    };
+    avy = {
+      flake = false;
+      owner = "abo-abo";
+      repo = "avy";
+      type = "github";
+    };
+    breadcrumb = {
+      flake = false;
+      owner = "joaotavora";
+      repo = "breadcrumb";
+      type = "github";
+    };
+    cape = {
+      flake = false;
+      owner = "minad";
+      repo = "cape";
+      type = "github";
+    };
+    casual = {
+      flake = false;
+      owner = "kickingvegas";
+      repo = "casual";
+      type = "github";
+    };
+    compat = {
+      flake = false;
+      owner = "emacs-compat";
+      repo = "compat";
+      type = "github";
+    };
+    consult = {
+      flake = false;
+      owner = "minad";
+      repo = "consult";
+      type = "github";
+    };
+    consult-dir = {
+      flake = false;
+      owner = "karthink";
+      repo = "consult-dir";
+      type = "github";
+    };
+    consult-gh = {
+      flake = false;
+      owner = "armindarvish";
+      repo = "consult-gh";
+      type = "github";
+    };
+    consult-gh-embark = {
+      flake = false;
+      owner = "armindarvish";
+      repo = "consult-gh";
+      type = "github";
+    };
+    corfu = {
+      flake = false;
+      owner = "minad";
+      repo = "corfu";
+      type = "github";
+    };
+    dash = {
+      flake = false;
+      owner = "magnars";
+      repo = "dash.el";
+      type = "github";
+    };
+    elisp-refs = {
+      flake = false;
+      owner = "Wilfred";
+      repo = "elisp-refs";
+      type = "github";
+    };
+    embark = {
+      flake = false;
+      owner = "oantolin";
+      repo = "embark";
+      type = "github";
+    };
+    embark-consult = {
+      flake = false;
+      owner = "oantolin";
+      repo = "embark";
+      type = "github";
+    };
+    "f" = {
+      flake = false;
+      owner = "rejeep";
+      repo = "f.el";
+      type = "github";
+    };
+    fontaine = {
+      flake = false;
+      owner = "protesilaos";
+      repo = "fontaine";
+      type = "github";
+    };
+    gcmh = {
+      flake = false;
+      owner = "koral";
+      repo = "gcmh";
+      type = "gitlab";
+    };
+    helpful = {
+      flake = false;
+      owner = "Wilfred";
+      repo = "helpful";
+      type = "github";
+    };
+    hide-mode-line = {
+      flake = false;
+      owner = "hlissner";
+      repo = "emacs-hide-mode-line";
+      type = "github";
+    };
+    marginalia = {
+      flake = false;
+      owner = "minad";
+      repo = "marginalia";
+      type = "github";
+    };
+    markdown-mode = {
+      flake = false;
+      owner = "jrblevin";
+      repo = "markdown-mode";
+      type = "github";
+    };
+    minions = {
+      flake = false;
+      owner = "tarsius";
+      repo = "minions";
+      type = "github";
+    };
+    mlscroll = {
+      flake = false;
+      owner = "jdtsmith";
+      repo = "mlscroll";
+      type = "github";
+    };
+    modus-themes = {
+      flake = false;
+      owner = "protesilaos";
+      repo = "modus-themes";
+      type = "github";
+    };
+    moody = {
+      flake = false;
+      owner = "tarsius";
+      repo = "moody";
+      type = "github";
+    };
+    nerd-icons = {
+      flake = false;
+      owner = "rainstormstudio";
+      repo = "nerd-icons.el";
+      type = "github";
+    };
+    nerd-icons-completion = {
+      flake = false;
+      owner = "rainstormstudio";
+      repo = "nerd-icons-completion";
+      type = "github";
+    };
+    nerd-icons-corfu = {
+      flake = false;
+      owner = "LuigiPiucco";
+      repo = "nerd-icons-corfu";
+      type = "github";
+    };
+    nerd-icons-dired = {
+      flake = false;
+      owner = "rainstormstudio";
+      repo = "nerd-icons-dired";
+      type = "github";
+    };
+    no-littering = {
+      flake = false;
+      owner = "emacscollective";
+      repo = "no-littering";
+      type = "github";
+    };
+    olivetti = {
+      flake = false;
+      owner = "rnkn";
+      repo = "olivetti";
+      type = "github";
+    };
+    orderless = {
+      flake = false;
+      owner = "oantolin";
+      repo = "orderless";
+      type = "github";
+    };
+    ox-gfm = {
+      flake = false;
+      owner = "larstvei";
+      repo = "ox-gfm";
+      type = "github";
+    };
+    page-break-lines = {
+      flake = false;
+      owner = "purcell";
+      repo = "page-break-lines";
+      type = "github";
+    };
+    popper = {
+      flake = false;
+      owner = "karthink";
+      repo = "popper";
+      type = "github";
+    };
+    posframe = {
+      flake = false;
+      owner = "tumashu";
+      repo = "posframe";
+      type = "github";
+    };
+    "s" = {
+      flake = false;
+      owner = "magnars";
+      repo = "s.el";
+      type = "github";
+    };
+    twist = {
+      flake = false;
+      owner = "emacs-twist";
+      repo = "twist.el";
+      type = "github";
+    };
+    vertico = {
+      flake = false;
+      owner = "minad";
+      repo = "vertico";
+      type = "github";
+    };
+    vertico-posframe = {
+      flake = false;
+      owner = "tumashu";
+      repo = "vertico-posframe";
+      type = "github";
+    };
+    yaml = {
+      flake = false;
+      owner = "zkry";
+      repo = "yaml.el";
+      type = "github";
+    };
+  };
+  outputs = { ... }: { };
+}
