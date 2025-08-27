5111c370330c482a3d6ce7db87e9220be5d43681
brklntmhwk <contact@younagi.dev>
2025-08-27 07:16:52 +0000

    feat(templates): add autoinsert template for dir-locals

---
+++ templates/autoinsert/dir-locals-insert.el
@@ -0,0 +1,5 @@
+;;; Directory local variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((org-mode . ((gac-debounce-interval . 180)
+							(eval . (git-auto-commit-mode t)))))
