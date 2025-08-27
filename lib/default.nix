5e6ef1fb99d8e48f5ee37242f6db420175b29c43
brklntmhwk <contact@younagi.dev>
2025-08-27 07:14:11 +0000

    style(lib): tweak indentation

--- lib/default.nix
+++ lib/default.nix
@@ -13,19 +13,19 @@
       twistArgs = {
         # https://github.com/akirak/emacs-config/commit/9940dc91e3ecf2b3faf861c2492867c9165202f3
         extraSiteStartElisp = ''
-          			(add-to-list 'treesit-extra-load-path "${
-               pkgs.emacs.pkgs.treesit-grammars.with-grammars (
-                 _:
-                 (pkgs.tree-sitter.override {
-                   extraGrammars = {
-                     tree-sitter-astro = {
-                       src = inputs.tree-sitter-astro.outPath;
-                     };
-                   };
-                 }).allGrammars
-               )
-             }/lib/")
-          		'';
+          (add-to-list 'treesit-extra-load-path "${
+            pkgs.emacs.pkgs.treesit-grammars.with-grammars (
+              _:
+              (pkgs.tree-sitter.override {
+                extraGrammars = {
+                  tree-sitter-astro = {
+                    src = inputs.tree-sitter-astro.outPath;
+                  };
+                };
+              }).allGrammars
+            )
+          }/lib/")
+        '';
         exportManifest = true; # Required to use hot-reloading twist.el offers
         initFiles = [ initFile ];
         initParser = inputs.twist.lib.parseUsePackages {
