77e06578cecbeb011fcb6586ba3d9bd109f2356b
Ohma Togaki <contact@younagi.dev>
2026-01-15 18:22:41 +0900

    (feat): add nix-maid module, remove nixos module, tweak hm module

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -3,57 +3,93 @@
 
 # https://github.com/akirak/emacs-config/commit/14ba1e6673c8c179f122b8bec2397b88dc74b04f
 
-# Take mkEmacsConfig as an arg to make additional configs based on user customization.
+# Take mkEmacsConfig as an arg to allow additional configs based on user customization.
 {
   inputs,
   mkEmacsConfig,
 }:
 {
   config,
+  options,
   lib,
   pkgs,
   ...
 }:
 
 let
+  inherit (builtins) attrValues removeAttrs;
   inherit (lib)
-    mkDefault
+    evalModules
+    mkEnableOption
     mkIf
+    mkMerge
     mkOption
     types
     ;
-  cfg = config.programs.emacs-twist;
+  cfg = config.programs.ametsuchi;
+
+  # Get the upstream (twist.nix) module options.
+  upstreamEval = evalModules {
+    modules = [
+      inputs.twist.homeModules.emacs-twist
+      {
+        config._module.check = false;
+      }
+    ];
+    specialArgs = {
+      inherit config lib pkgs;
+    };
+  };
+  twistOpts = upstreamEval.options.programs.emacs-twist;
+
+  # Exclude these options since they are overridden or their values are set
+  # on this Ametsuchi layer.
+  excludedOptions = [
+    "enable"
+    "createInitFile"
+    "createManifestFile"
+    "manifestFileName"
+    "earlyInitFile"
+    "config"
+  ];
+  inheritedOptions = removeAttrs twistOpts excludedOptions;
 in
 {
   imports = [
     inputs.twist.homeModules.emacs-twist
   ];
 
-  options.programs.emacs-twist = {
-    settings = {
-      extraFeatures = mkOption {
-        type = types.listOf types.str;
-        description = "Add extra features";
-        default = [ ];
-      };
+  options.programs.ametsuchi = inheritedOptions // {
+    enable = mkEnableOption "Ametsuchi";
+    extraFeatures = mkOption {
+      type = types.listOf types.str;
+      description = "Add extra features";
+      default = [ ];
     };
   };
 
   config = mkIf cfg.enable {
-    programs.emacs-twist = {
-      emacsclient.enable = true;
-      serviceIntegration.enable = mkDefault true;
-      createInitFile = true;
-      createManifestFile = true;
-      directory = ".local/share/emacs"; # Consider it more of a datum than a config.
-      earlyInitFile = ../early-init.el;
-      config = mkEmacsConfig {
-        inherit pkgs;
-        features = cfg.settings.extraFeatures;
-      };
-    };
+    programs.emacs-twist =
+      let
+        inherit (builtins) attrNames;
+        inherit (lib) genAttrs mkAliasDefinitions;
+        sharedKeys = attrNames inheritedOptions;
+      in
+      mkMerge [
+        # e.g., `{ name = mkAliasDefinitions options.programs.ametsuchi.name; ... }`
+        (genAttrs sharedKeys (name: mkAliasDefinitions options.programs.ametsuchi.${name}))
+        {
+          enable = true;
+          createInitFile = true;
+          earlyInitFile = ../early-init.el;
+          config = mkEmacsConfig {
+            inherit pkgs;
+            features = cfg.extraFeatures;
+          };
+        }
+      ];
 
-    home.packages = builtins.attrValues {
+    home.packages = attrValues {
       # Add font packages that will be used in your Emacs config.
       inherit (pkgs)
         moralerspace-hwnf
---
+++ modules/nix-maid.nix
@@ -0,0 +1,207 @@
+# Copyright (C) 2025 Ohma Togaki
+# SPDX-License-Identifier: MIT
+
+# Based on:
+# https://github.com/emacs-twist/twist.nix/commit/b566be881ee812b646f4201d18d1424d0f5127c6
+
+# Take mkEmacsConfig as an arg to allow additional configs based on user customization.
+mkEmacsConfig:
+{
+  config,
+  options,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  inherit (builtins) attrValues concatStringsSep listToAttrs;
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    mkMerge
+    mkOption
+    mkOptionType
+    optional
+    optionalString
+    types
+    ;
+  inherit (pkgs) makeDesktopItem runCommandLocal;
+  cfg = config.programs.ametsuchi;
+
+  emacsConfig = mkEmacsConfig {
+    inherit pkgs;
+    features = cfg.extraFeatures;
+  };
+
+  initFile = runCommandLocal "init.el" { } ''
+    mkdir -p $out
+    touch $out/init.el
+    for file in ${concatStringsSep " " emacsConfig.initFiles}
+    do
+      cat "$file" >> $out/init.el
+      echo >> $out/init.el
+    done
+  '';
+
+  wrappedEmacs =
+    runCommandLocal cfg.name
+      {
+        propagatedBuildInputs = [
+          emacsConfig
+        ];
+      }
+      ''
+        mkdir -p $out/bin
+
+        # Use "exec" instead of "makeWrapper" to allow safe runtime expansion
+        # of variables; it wraps arguments in single quotes, which prevents
+        # the shell from expanding runtime variables.
+        cat > $out/bin/${cfg.name} <<EOF
+        #!/bin/sh
+        exec ${emacsConfig}/bin/emacs \
+        --init-directory="\$HOME/${cfg.directory}" "\$@"
+        EOF
+
+        chmod +x $out/bin/${cfg.name}
+
+        ${lib.optionalString cfg.emacsclient.enable "ln -t $out/bin -s ${emacsConfig.emacs}/bin/emacsclient"}
+      '';
+
+  fonts = attrValues {
+    # Add font packages that will be used in your Emacs config.
+    inherit (pkgs)
+      moralerspace-hwnf
+      sarasa-gothic
+      noto-fonts-emoji
+      symbola
+      ;
+  };
+
+  desktopItem = makeDesktopItem {
+    inherit (cfg) name;
+    inherit (cfg.desktopItem) desktopName mimeTypes;
+    comment = "Edit text";
+    genericName = "Text Editor";
+    exec = "${cfg.name} %F";
+    icon = "emacs";
+    startupNotify = true;
+    startupWMClass = "Emacs";
+    categories = [
+      "TextEditor"
+      "Development"
+    ];
+  };
+
+  # https://github.com/viperML/nix-maid/commit/4ea39e76cdc8f8946bf4474a55962b2dfd8258fb
+  userSubmodule = {
+    config = mkIf cfg.enable {
+      packages = [
+        fonts
+        wrappedEmacs
+      ]
+      ++ optional cfg.icons.enable emacsConfig.icons
+      ++ optional (!pkgs.stdenv.isDarwin) desktopItem;
+
+      maid = {
+        file = {
+          home = listToAttrs [
+            {
+              name = "${cfg.directory}/init.el";
+              value = {
+                source = "${initFile}/init.el";
+              };
+            }
+            {
+              name = "${cfg.directory}/templates";
+              value = {
+                source = ../templates;
+              };
+            }
+            {
+              name = "${cfg.directory}/early-init.el";
+              value = {
+                source = ../early-init.el;
+              };
+            }
+          ];
+        };
+      };
+    };
+  };
+in
+{
+  options = {
+    users.users = mkOption {
+      type = types.attrsOf (types.submodule userSubmodule);
+    };
+
+    programs.ametsuchi = {
+      enable = mkEnableOption "Ametsuchi";
+      name = mkOption {
+        type = types.str;
+        description = "Name of the wrapper script.";
+        default = "emacs";
+        example = "my-emacs";
+      };
+      directory = mkOption {
+        type = types.str;
+        description = "Relative path in string to user-emacs-directory from the home directory";
+        default = ".config/emacs";
+        example = ".local/share/emacs";
+      };
+      emacsclient = {
+        enable = mkEnableOption "emacsclient";
+      };
+      serviceIntegration = {
+        enable = mkEnableOption (
+          lib.mdDoc ''
+            Enable service integration. For now, only systemd is supported.
+          ''
+        );
+      };
+      icons = {
+        enable = mkOption {
+          type = types.bool;
+          description = "Whether to install Emacs icons.";
+          default = true;
+        };
+      };
+      desktopItem = {
+        desktopName = mkOption {
+          type = types.str;
+          description = "Long name of the desktop item.";
+          default = "Emacs";
+        };
+        mimeTypes = mkOption {
+          type = types.listOf types.str;
+          description = "List of mime types associated with the wrapper.";
+          default = [
+            "text/plain"
+            "inode/directory"
+          ];
+        };
+      };
+      extraFeatures = mkOption {
+        type = types.listOf types.str;
+        description = "Add extra features";
+        default = [ ];
+      };
+      packageWrapped = mkOption {
+        type = types.package;
+        description = "The wrapped Emacs package.";
+        readOnly = true;
+        default = wrappedEmacs;
+      };
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.emacs = mkIf cfg.serviceIntegration.enable {
+      enable = true;
+      # Generate a desktop file for emacsclient.
+      client.enable = true;
+      package = wrappedEmacs;
+    };
+  };
+}
