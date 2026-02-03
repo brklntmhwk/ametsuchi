91aeed70397edb84dbc0d78ac382f6333100b7d5
Ohma Togaki <contact@younagi.dev>
2026-02-03 14:49:23 +0900

    fix: add location for native compiled elc files to early-init.el

--- early-init.el
+++ early-init.el
@@ -1,14 +1,31 @@
 ;;; early-init.el -*- lexical-binding: t; -*-
 
-;;; Startup
+;;;; Startup ;;;;
 
 ;; Disable package.el in preparation for using emacs-twist instead.
-(setq package-enable-at-startup nil)
-(setq package-quickstart nil)
+(with-eval-after-load 'package
+  (setopt package-enable-at-startup nil
+          package-quickstart nil))
 
 (advice-add 'x-apply-session-resources :override 'ignore)
 
-;;; UI display control
+;;;; Native Compile ;;;;
+
+;; https://apribase.net/2024/07/09/emacs-eln-cache/
+(when (boundp 'native-comp-eln-load-path)
+  ;; Redirect the cache to a clean XDG location.
+  (startup-redirect-eln-cache
+   (expand-file-name "~/.local/share/emacs/eln-cache/")))
+
+(with-eval-after-load 'comp
+  (setopt native-comp-async-jobs-number 8
+          native-comp-speed 1
+          native-comp-always-compile t))
+
+(with-eval-after-load 'warnings
+  (setopt warning-suppress-types '((comp))))
+
+;;;; UI Display Control ;;;;
 
 (push '(menu-bar-lines . 0) default-frame-alist)
 (push '(tool-bar-lines . 0) default-frame-alist)
