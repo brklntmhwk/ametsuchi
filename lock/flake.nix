4554b440d36d193b9aa09b5b86def4cc3b71c688
brklntmhwk <86272619+brklntmhwk@users.noreply.github.com>
2026-04-10 21:29:47 +0000

    [create-pull-request] automated change

--- lock/flake.nix
+++ lock/flake.nix
@@ -366,7 +366,7 @@
     };
     highlight-indent-guides = {
       flake = false;
-      owner = "DarthFennec";
+      owner = "bumblepup";
       repo = "highlight-indent-guides";
       type = "github";
     };
