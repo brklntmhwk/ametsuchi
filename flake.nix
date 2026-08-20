e594f8235bb81b1cb371a1b5d2efa6091c4edb5a
Ohma Togaki <contact@younagi.dev>
2026-08-21 00:08:06 +0900

    chore: update lockfile

--- flake.nix
+++ flake.nix
@@ -21,7 +21,7 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "git+https://github.com/nixos/nixpkgs?shallow=1&ref=nixos-unstable";
 
     # Nix utils
     nix-filter.url = "github:numtide/nix-filter";
@@ -35,9 +35,7 @@
     # Emacs-overlay
     emacs-overlay = {
       url = "github:nix-community/emacs-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     # Emacs-twist
